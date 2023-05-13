8dce2e867390294a53108bc735ea74027df4845d
ekrauseb <54483060+ekrauseb@users.noreply.github.com>
2023-05-13 15:19:28 +0200

    Update index.js

--- index.js
+++ index.js
@@ -7,13 +7,6 @@ const cors = require('cors');
 
 app.use(cors()); 
 
-app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', "*");
-  res.header('Access-Control-Allow-Headers', true);
-  res.header('Access-Control-Allow-Credentials', true);
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  next();
-});
 
 app.use(bodyParser.json())
 app.use(
